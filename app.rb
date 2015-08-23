09a185fdc36aec3303aa3b9ae402aee0fafcf8b5
kota.tome <yk.ltw.w2@gmail.com>
2015-08-23 16:49:28 +0900

    Staging環境作成と設定の追加

--- app.rb
+++ app.rb
@@ -1,5 +1,12 @@
 require 'sinatra'
 
+configure :staging do
+  use Rack::Auth::Basic do |username, password|
+    username == ENV['STAGING_BASIC_AUTH_USERNAME'] && 
+      password == ENV['STAGING_BASIC_AUTH_PASSWORD']
+  end
+end
+
 get '/' do
   "Hello, World!!"
 end
