bcb5853ac45781c2f4d7a8d2e23ada008e229fba
kota.tome <yk.ltw.w2@gmail.com>
2015-09-06 17:50:57 +0900

    Redis に最終アクセス時刻をキャッシュさせてみた

--- app.rb
+++ app.rb
@@ -1,4 +1,6 @@
+# -*- coding: utf-8 -*-
 require 'sinatra'
+require 'redis'
 
 configure :staging do
   use Rack::Auth::Basic do |username, password|
@@ -7,6 +9,19 @@ configure :staging do
   end
 end
 
+redis = Redis.new(host:"127.0.0.1", port:"6379")
+
 get '/' do
-  "Hello, World!!"
+  # Redis に最終アクセス時刻をキャッシュ
+  last_access_at = nil
+  last_access_at = redis.get("LAST_ACCESS_AT")
+  if last_access_at == nil then
+    last_access_at = Time.now
+  end
+  redis.set("LAST_ACCESS_AT", Time.now)
+
+  # 画面表示
+  "Hello, World!! <br/>" +
+    "Last access at: " + last_access_at.to_s + "<br/>" +
+    "Current access at :" + Time.now.to_s + "<br/>"
 end
