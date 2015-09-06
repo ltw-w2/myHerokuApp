4fa3d0c5f576a11b9db33d5cca7ee5d986645f36
kota.tome <yk.ltw.w2@gmail.com>
2015-09-06 21:02:42 +0900

    ローカルとステージング環境で DB 向き先を変える

--- app.rb
+++ app.rb
@@ -2,14 +2,35 @@
 require 'sinatra'
 require 'redis'
 
+configure :development, :test do
+  set :REDIS_HOST     => "127.0.0.1"
+  set :REDIS_PORT     => "6379"
+  set :REDIS_PASSWORD => nil
+end
+
 configure :staging do
+  # BASIC認証
   use Rack::Auth::Basic do |username, password|
     username == ENV['STAGING_BASIC_AUTH_USERNAME'] && 
       password == ENV['STAGING_BASIC_AUTH_PASSWORD']
   end
+  # Redis設定
+  set :REDIS_HOST     => ENV['STAGING_REDIS_HOST']
+  set :REDIS_PORT     => ENV['STAGING_REDIS_PORT']
+  set :REDIS_PASSWORD => ENV['STAGING_REDIS_PASSWORD']
 end
 
-redis = Redis.new(host:"127.0.0.1", port:"6379")
+
+# Redis
+if settings.REDIS_PASSWORD != nil then
+  redis = Redis.new(:host => settings.REDIS_HOST,
+                    :port => settings.REDIS_PORT,
+                    :password => settings.REDIS_PASSWORD)
+else
+  redis = Redis.new(:host => settings.REDIS_HOST,
+                    :port => settings.REDIS_PORT)
+end
+
 
 get '/' do
   # Redis に最終アクセス時刻をキャッシュ
